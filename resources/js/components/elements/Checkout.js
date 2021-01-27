a98053c8faec900e17f7aadf99f5c18b96ba7267
esponges <esponges@gmail.com>
2021-01-27 17:41:07 -0600

    check out laravel and pop below cart

--- resources/js/components/elements/Checkout.js
+++ resources/js/components/elements/Checkout.js
@@ -1,23 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Form, Button, Row, Col, Jumbotron, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Checkout = (props) => {
+    const [phone, setPhone] = useState("Ingresa tu número telefónico");
+    const [address, setAddress] = useState("");
+
+    const pay = (e) => {
+        e.preventDefault();
+        axios.
+        post('order/create', {
+            address: address,
+            payment_mode: 'paypal'
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    }
+
+    useEffect(() => {
+        props.userInfo[3] && setPhone(props.userInfo[3]);
+    })
+
     return (
         <div>
-            {console.log(props)}
             {props.loggedIn ? (
                 <div>
-                    <Form>
+                    {/* use laravel form method */}
+                    <Form className="mt-3" action="/order/create" method="post">
+                        {/* place csrf token */}
+                        <input type="hidden" value={csrf_token} name="_token" />
+                        <input type="hidden" value="paypal" name="payment_mode" />
                         <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email</Form.Label>
+                            <Form.Label>Tu nombre</Form.Label>
                             <Form.Control
-                                type="email"
-                                placeholder="Ingresa correo"
+                                type="text"
+                                placeholder={`${props.userInfo[1]}`}
+                                disabled
                             />
-                            <Form.Text className="text-muted">
+                            {/* <Form.Text className="text-muted">
                                 Aquí te llegará tu confirmación
-                            </Form.Text>
+                            </Form.Text> */}
+                            <Form.Label>Tu teléfono</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder={`${phone}`}
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                            />
+                            {/* <Form.Text className="text-muted">
+                                Aquí te llegará tu confirmación
+                            </Form.Text> */}
                         </Form.Group>
                         <Form.Label>Tu CP</Form.Label>
                         <Form.Control as="select">
@@ -32,9 +69,10 @@ const Checkout = (props) => {
                             <Form.Control
                                 type="text"
                                 placeholder="La dirección de tu casa"
+                                name="address"
                             />
                         </Form.Group>
-                        <Form.Group as={Row}>
+                        {/* <Form.Group as={Col}>
                             <Form.Label as="legend" column sm={2}>
                                 Método de pago
                             </Form.Label>
@@ -52,8 +90,8 @@ const Checkout = (props) => {
                                     id="paypalPayment"
                                 />
                             </Col>
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
+                        </Form.Group> */}
+                        <Button className="mb-5" variant="primary" type="submit">
                             Proceder a pago
                         </Button>
                     </Form>
