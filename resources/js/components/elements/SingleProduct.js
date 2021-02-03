6e22efaec493429fe01933213ea370fc5a4921a5
esponges <esponges@gmail.com>
2021-02-02 20:14:31 -0600

    Update SingleProduct.js
    Added its own useEffect in case of refresh or direct link. It does not relies any more of parent props.
    It now displays correct img.
    Buy btn added.

--- resources/js/components/elements/SingleProduct.js
+++ resources/js/components/elements/SingleProduct.js
@@ -1,34 +1,82 @@
-import React from 'react';
-import Footer from '../index/Footer';
-import IndexNavbar from '../index/IndexNavbar';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 import { withRouter } from 'react-router';
 
 const SingleProduct = (props) => {
-    const product = props.location.state;
+    const [itemCount, setItemCount] = useState(1);
+    // const product = props.location.state;
+    const [product, setProduct] = useState({})
+
+    const addToCart = (id, event) => {
+        event.preventDefault();
+        console.log(`item ${id}, cantidad ${itemCount}`);
+        axios
+            .get(`cart/${id}/add/${itemCount}`)
+            .then((res) => {
+                props.cartCountUpdate(itemCount);
+                window.alert(res.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
+
+    useEffect(() => {
+        // get product info
+        axios
+        .get(`/products/${props.match.params.id}`)
+        .then(res => {
+            setProduct(res.data);
+        })
+        .catch((err) => {
+            console.error(err)
+        });
+    }, [])
 
     return (
         <>
-            {console.log(props)}
-            {product ? (
-                <Card style={{ width: "18rem", marginTop: "10%" }}>
-                    <Card.Img variant="top" src={`/img/${props.match.params.id}.jpg`} />
+            {console.log(product)}
+            {product != {} && (
+                <Card style={{ width: "32rem", marginTop: "10%", marginBottom: "10%" }}>
+                    <Card.Img
+                        variant="top"
+                        src={product.id && `/img/${product.id}.jpg`}
+                    />
                     <Card.Body>
                         <Card.Title>{product.name}</Card.Title>
-                        <Card.Text>
-                            {product.description}
-                        </Card.Text>
+                        <Card.Text>{product.description}</Card.Text>
                     </Card.Body>
                     <ListGroup className="list-group-flush">
                         <ListGroupItem>Precio: $ {product.price}</ListGroupItem>
-                        <ListGroupItem>Compara precios: Walmart/Consuvino</ListGroupItem>
+                        <ListGroupItem>
+                            Compara precios: Walmart/Consuvino
+                        </ListGroupItem>
                     </ListGroup>
                     <Card.Body>
-                        <Card.Link href="#">Card Link</Card.Link>
+                        <div className="row">
+                            <div className="col-6">
+                                <input
+                                    type="number"
+                                    name="quantity"
+                                    defaultValue={1}
+                                    className="form-control input-number"
+                                    onChange={async (e) =>
+                                        await setItemCount(e.target.value)
+                                    }
+                                    style={{ minWidth: "60px" }}
+                                />
+                            </div>
+                            <div className="col-6">
+                                <Button
+                                    variant="primary"
+                                    onClick={() => addToCart(product.id, event)}
+                                >
+                                    +
+                                </Button>
+                            </div>
+                        </div>
                     </Card.Body>
                 </Card>
-            ) : (
-                ""
             )}
         </>
     );
