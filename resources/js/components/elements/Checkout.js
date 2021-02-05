f839820124c9da285abbb0d95e047ad6fbc8d28d
esponges <esponges@gmail.com>
2021-02-05 15:16:43 -0600

    checkout validations frontend

--- resources/js/components/elements/Checkout.js
+++ resources/js/components/elements/Checkout.js
@@ -1,14 +1,55 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Form, Button, Row, Col, Jumbotron, ListGroup } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Form, Button, Alert } from "react-bootstrap";
+import LoginOrRegister from '../auth/LoginOrRegister';
 
 const Checkout = (props) => {
-    const [phone, setPhone] = useState("Ingresa tu número telefónico");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    const [addressDetails, setAddressDetails] = useState("");
+    const [buttonIsActive, setButtonIsActive] = useState(false);
+    const [phoneAlertMessage, setPhoneAlertMessage] = useState(null);
+    const [addressAlertMessage, setAddressAlertMessage] = useState(null);
 
     useEffect(() => {
         props.userInfo[3] && setPhone(props.userInfo[3]);
-    })
+    }, []);
+
+    // validate
+    useEffect(() => {
+        //validate phone
+        const phonePattern = new RegExp(/^[0-9\b]+$/);
+        if (phone) {
+            if (phonePattern.test(phone)) {
+                if (phone.length === 10) {
+                    setPhoneAlertMessage(false);
+                } else {
+                    setPhoneAlertMessage(
+                        "Por favor ingresa número de 10 dígitos"
+                    );
+                }
+            } else {
+                setPhoneAlertMessage("Por favor sólo ingresa números");
+            }
+        }
+        //validate address
+        if (address.length < 8 && address != "") {
+            setAddressAlertMessage("Por favor ingresa dirección correcta");
+        } else {
+            setAddressAlertMessage(false);
+        }
+
+        // activate proceed button
+        if (address.length > 8 && phonePattern.test(phone) && phone.length == 10)
+            setButtonIsActive(true);
+        else setButtonIsActive(false);
+    }, [address, phone]);
+
+
+    // validate phone
+    useEffect(() => {
+
+
+    }, [phone]);
 
     return (
         <div>
@@ -18,7 +59,11 @@ const Checkout = (props) => {
                     <Form className="mt-3" action="/order/create" method="post">
                         {/* place csrf token */}
                         <input type="hidden" value={csrf_token} name="_token" />
-                        <input type="hidden" value="paypal" name="payment_mode" />
+                        <input
+                            type="hidden"
+                            value="paypal"
+                            name="payment_mode"
+                        />
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Tu nombre</Form.Label>
                             <Form.Control
@@ -26,20 +71,21 @@ const Checkout = (props) => {
                                 placeholder={`${props.userInfo[1]}`}
                                 disabled
                             />
-                            {/* <Form.Text className="text-muted">
-                                Aquí te llegará tu confirmación
-                            </Form.Text> */}
-                            <Form.Label>Tu teléfono</Form.Label>
+
+                            <Form.Label className="mt-2">Tu teléfono</Form.Label>
                             <Form.Control
                                 type="text"
-                                placeholder={`${phone}`}
-                                value={phone}
+                                placeholder="ingresa tu teléfono"
+                                value={phone && phone}
                                 onChange={(e) => setPhone(e.target.value)}
                             />
-                            {/* <Form.Text className="text-muted">
-                                Aquí te llegará tu confirmación
-                            </Form.Text> */}
+                            {phoneAlertMessage && (
+                                <Alert variant={"warning"} className="m-1">
+                                    {phoneAlertMessage}
+                                </Alert>
+                            )}
                         </Form.Group>
+
                         <Form.Label>Tu CP</Form.Label>
                         <Form.Control as="select">
                             <option>1</option>
@@ -48,62 +94,50 @@ const Checkout = (props) => {
                             <option>4</option>
                             <option>5</option>
                         </Form.Control>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Dirección completa</Form.Label>
+
+                        {/* address */}
+                        <Form.Group controlId="formBasicPassword" className="mt-2">
+                            <Form.Label>Calle y número exterior <i>(e interior si tienes)</i></Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder="La dirección de tu casa"
+                                value={address}
                                 name="address"
+                                onChange={(e) => {setAddress(e.target.value)}}
                             />
                         </Form.Group>
-                        {/* <Form.Group as={Col}>
-                            <Form.Label as="legend" column sm={2}>
-                                Método de pago
-                            </Form.Label>
-                            <Col sm={10}>
-                                <Form.Check
-                                    type="radio"
-                                    label="Pago contra entrega"
-                                    name="paymentType"
-                                    id="cashPayment"
-                                />
-                                <Form.Check
-                                    type="radio"
-                                    label="Pago con Paypal"
-                                    name="paymentType"
-                                    id="paypalPayment"
-                                />
-                            </Col>
-                        </Form.Group> */}
-                        <Button className="mb-5" variant="primary" type="submit">
-                            Proceder a pago
-                        </Button>
+                        {addressAlertMessage && (
+                            <Alert variant={"warning"} className="m-1">
+                                {addressAlertMessage}
+                            </Alert>
+                        )}
+
+                        {/* more address info */}
+                        <Form.Group controlId="formBasicPassword" className="mt-2">
+                            <Form.Label>Opcional - Condominio, faccionamiento, o edificio <i>(detalles)</i></Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Para dar más fácilmente contigo"
+                                name="address_details"
+                                value={addressDetails}
+                                onChange={(e) => {setAddressDetails(e.target.value)}}
+                            />
+                        </Form.Group>
+
+                        {/* let user pay if all information is set */}
+                        {buttonIsActive && (
+                            <Button
+                                className="mb-5"
+                                variant="primary"
+                                type="submit"
+                            >
+                                Proceder a pago
+                            </Button>
+                        )}
                     </Form>
                 </div>
             ) : (
-                <div>
-                    <Jumbotron>
-                        <h1>Regístrate</h1>
-                        <ListGroup>
-                            <ListGroup.Item>
-                                Sólo necesitas un correo electrónico válido.
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                O si ya estás registrado, inicia sesión.
-                            </ListGroup.Item>
-                        </ListGroup>
-                        <div className="container mt-3">
-                            <Link to="/register">
-                                <Button>Regístrate</Button> &nbsp;&nbsp;&nbsp;
-                            </Link>
-                            <Link to="/login">
-                                <Button variant={"secondary"} size='sm'>
-                                    Inicia sesión
-                                </Button>
-                            </Link>
-                        </div>
-                    </Jumbotron>
-                </div>
+                <LoginOrRegister className="container mt-2" />
             )}
         </div>
     );
