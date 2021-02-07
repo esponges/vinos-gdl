90ca74c8fa5b334d99507c4166f23af8ab02261a
esponges <esponges@gmail.com>
2021-02-07 11:31:23 -0600

    somes tests with jest

---
+++ resources/js/tests/MainJumbo.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow, mount, configure } from "enzyme";
+import toJson from "enzyme-to-json";
+import Adapter from "enzyme-adapter-react-16";
+import { BrowserRouter as Router } from "react-router-dom";
+import { create, update } from "react-test-renderer";
+import MainJumbo from "../components/index/MainJumbo";
+
+configure({ adapter: new Adapter() });
+
+describe("mainjumbo has what it has", () => {
+    it("has 3 p", () => {
+        const tree = shallow(<MainJumbo />);
+        expect(tree.find("h1").length).toEqual(1);
+        expect(tree.find("p").length).toEqual(3);
+    });
+});
