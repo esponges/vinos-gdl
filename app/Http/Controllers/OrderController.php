e8bddb5aba8d8c164a9c4a0fa089e0abc9a4faf1
esponges <esponges@gmail.com>
2021-04-21 14:44:11 -0500

    adjust order contoller, testa and extras

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TransferOrderConfirmation;
+use App\Mail\AdminOrderConfirmationEmail;
 use GuzzleHttp\Exception\BadResponseException;
 
 class OrderController extends Controller
@@ -85,7 +86,6 @@ class OrderController extends Controller
             $order,
             $products,
             $grandTotal,
-            $cartTotal,
             $balanceToPay,
         ));
 
@@ -102,7 +102,7 @@ class OrderController extends Controller
                 $order,
                 $products,
                 $grandTotal,
-                $cartTotal,
+                $cartTotal, // === $paidWithPayPal
                 $balanceToPay
             ));
         }
@@ -124,5 +124,77 @@ class OrderController extends Controller
         );
 
         return $cartItems;
+    }
+
+    /* V1 PAYPAL PAYMENT METHODS */
+
+    public function create(Request $request)
+    {
+        if (Auth::user()) {
+            $order = new Order();
+            $order->total = \Cart::getTotal();
+            $order->total_items = \Cart::getTotalQuantity();
+            $order->user_id = Auth::user()->id;
+            $order->order_name = $request->order_name;
+            $order->payment_mode = $request->payment_mode;
+            $order->address = $request->address;
+            $order->address_details = $request->address_details;
+            $order->delivery_day = $request->delivery_day;
+            $order->delivery_schedule = $request->delivery_schedule;
+            $order->phone = $request->phone;
+            $order->cp = $request->cp;
+            $order->neighborhood = $request->neighborhood;
+
+            $order->save();
+
+            $cartItems = \Cart::getContent();
+
+            foreach ($cartItems as $item) {
+                DB::table('order_items')->insert([
+                    'product_id' => $item->id,
+                    'unit_price' => $item->price,
+                    'order_id' => $order->id,
+                    'qty' => $item->quantity,
+                ]);
+            }
+
+            if ($order->payment_mode == "transfer") {
+                // return $this->transferPaymentMode($order);
+                return redirect()->route('paypal.transfer', $order->id);
+            };
+
+            return redirect()->route('paypal.checkout', [$order->id, $order->payment_mode]);
+        }
+        return response()->json('session timed out', 408);
+    }
+
+    public function transferPaymentMode($orderId)
+    {
+        // prepare content
+        $order = Order::find($orderId);
+        $products = \Cart::getContent();
+        $grandTotal = \Cart::getTotal();
+        $cartTotal = 0;
+        $balanceToPay = $grandTotal - $cartTotal;
+        $user = auth()->user();
+
+        \Cart::clear();
+
+        //send email
+        $this->prepareConfirmationEmails($user, $order, $products, $grandTotal, $cartTotal, $balanceToPay);
+
+        return view('order.success', compact('order', 'orderId', 'products', 'grandTotal', 'cartTotal', 'balanceToPay'));
+    }
+
+    public function orderSuccess(Request $request, $orderId, $cartTotal)
+    {
+        $order = Order::find($orderId);
+        $products = \Cart::getContent();
+        $grandTotal = \Cart::getTotal();
+        $balanceToPay = $grandTotal - $cartTotal;
+
+        \Cart::clear();
+
+        return view('order.paypalSuccess', compact('order', 'products', 'grandTotal', 'balanceToPay', 'cartTotal'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,24 +14,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     {{-- SEO --}}
     <title>Vinoreomx</title>
     <meta name="description" content="Tienda online. Entrega gratis de vinos y licores a domicilio en Guadalajara. Precios de mayoreo con la mayor selección de tequila, whisky, gin, vodka y más.">
 
     @php
-        use Srmklive\PayPal\Facades\PayPal as PayPalClient;
 
         $host = $_SERVER['SERVER_NAME'];
         // if ($host === '127.0.0.1') echo $host;
+
         /* PayPal Access Token with Srmk-Laravel */
-        PayPalClient::setProvider();
-        $paypalProvider = PayPalClient::getProvider();
-        $paypalProvider->setApiCredentials(config('paypal'));
-        $access_token = $paypalProvider->getAccessToken()['access_token'];
+        // use Srmklive\PayPal\Facades\PayPal as PayPalClient;
+        // PayPalClient::setProvider();
+        // $paypalProvider = PayPalClient::getProvider();
+        // $paypalProvider->setApiCredentials(config('paypal'));
+        // $access_token = $paypalProvider->getAccessToken()['access_token'];
+
     @endphp
 
-    <meta name='paypaltoken' content ='{{ $access_token }}'>
+    {{-- <meta name='paypaltoken' content ='{{ $access_token }}'> --}}
 
     <link type="text/css" href={{ $host == '127.0.0.1' ? asset('css/app.css') : '/css/app.css' }} rel="stylesheet">
 
