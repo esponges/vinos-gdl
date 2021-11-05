70e419eb64bcc2f1959c72e350f40f850762616e
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2021-11-04 19:56:23 -0600

    unnecesary fetch requests

--- resources/js/components/controls/hooks/misc.js
+++ resources/js/components/controls/hooks/misc.js
@@ -2,40 +2,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const useEffectProducts = () => {
-    const [error, setError] = useState(null);
-    const [products, setProducts] = useState(null);
-    const [prods, setProds] = useState("");
     const [cartCount, setCartCount] = useState(0);
-    const [cart, setCart] = useState(null);
 
     useEffect(() => {
-        axios
-            .get("/categories")
-            .then((res) => {
-                setProducts(res.data);
-            })
-            .catch((err) => {
-                setError(err.message);
-            });
-
-        axios
-            .get("/products")
-            .then((res) => {
-                setProds(res.data);
-            })
-            .catch((err) => {
-                setError(err.message);
-            });
-
         axios.get("/cart/count").then((res) => {
             setCartCount(res.data[0]);
         });
     }, []);
 
     return {
-        products,
-        prods,
-        error,
         cartCount,
         setCartCount,
     };
