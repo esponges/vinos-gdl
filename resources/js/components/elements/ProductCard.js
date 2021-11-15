d1c1e4df77ae2abaad89251ebb7633489a723254
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2021-11-14 18:13:59 -0600

    lint

--- resources/js/components/elements/ProductCard.js
+++ resources/js/components/elements/ProductCard.js
@@ -1,97 +1,99 @@
-import React, { useContext } from "react";
-import { Button, Card, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingBag, fas } from "@fortawesome/free-solid-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { useDispatch } from "react-redux";
-import { addItemToCart } from "../../store/cart/reducers";
-import { Context } from "../Context";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBag, fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { useDispatch } from 'react-redux';
+import { addItemToCart } from '../../store/cart/reducers';
+import { Context } from '../Context';
+
 library.add(fas);
 
 const ProductCard = React.memo(({ product, setItemCount, itemCount }) => {
-    const dispatch = useDispatch();
-    const context = useContext(Context);
+  const dispatch = useDispatch();
+  const context = useContext(Context);
 
-    const handleAddItemToCart = (e, id, price) => {
-        e.preventDefault();
+  const handleAddItemToCart = (e, id, price) => {
+    e.preventDefault();
 
-        dispatch(addItemToCart(id, itemCount, price));
-        const subTotal = price * itemCount;
-        context.notifyMinAmountRemaining(subTotal);
-    }
+    dispatch(addItemToCart(id, itemCount, price));
+    const subTotal = price * itemCount;
+    context.notifyMinAmountRemaining(subTotal);
+  };
 
-    return (
-        <div
-            key={product.id}
-            className="col-lg-3 col-md-4 mt-3"
-            id="product-card-mobile"
+  return (
+    <div
+      key={product.id}
+      className="col-lg-3 col-md-4 mt-3"
+      id="product-card-mobile"
+    >
+      <Card>
+        <Link
+          to={{
+            pathname: `/products/${product.id}`,
+          }}
         >
-            <Card>
-                <Link
-                    to={{
-                        pathname: `/products/${product.id}`,
-                    }}
-                >
-                    <Card.Img
-                        variant="top"
-                        src={`/img/products/${product.id}.jpg`}
-                    />
-                </Link>
-                <Card.Body>
-                    <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>
-                        <b>
-                            {new Intl.NumberFormat("en-US", {
-                                style: "currency",
-                                currency: "MXN",
-                            }).format(product.price)}
-                        </b>
-                    </Card.Text>
-                    {product.comp_price && (
-                        <Card.Text id="grid-competitor-price">
-                            Prom. competencia: ${product.comp_price}*
-                        </Card.Text>
-                    )}
-                    <div className="btn-group">
-                        <div className="d-none d-xl-block">
-                            <input
-                                type="number"
-                                min="1"
-                                name="quantity"
-                                value={itemCount}
-                                className="form-control input-number"
-                                onChange={async (e) =>
-                                    await setItemCount(parseInt(e.target.value))
-                                }
-                                style={{
-                                    maxWidth: "70px",
-                                }}
-                            />
-                        </div>
-                        <Button
-                            variant="primary"
-                            id="main-add-btn"
-                            data-testid={`main-add-btn-${product.id}`}
-                            onClick={(e) =>
-                                handleAddItemToCart(e, product.id, product.price)
-                            }
-                        >
-                            Añadir <FontAwesomeIcon icon={faShoppingBag} />
-                        </Button>
-                        <Link
-                            to={{
-                                pathname: `/products/${product.id}`,
-                            }}
-                            id="main-details-btn"
-                        >
-                            <Button variant="secondary">Info</Button>
-                        </Link>
-                    </div>
-                </Card.Body>
-            </Card>
-        </div>
-    );
+          <Card.Img
+              variant="top"
+              src={`/img/products/${product.id}.jpg`}
+          />
+        </Link>
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>
+            <b>
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'MXN',
+              }).format(product.price)}
+            </b>
+          </Card.Text>
+          {product.comp_price && (
+            <Card.Text id="grid-competitor-price">
+              Prom. competencia: $
+              {product.comp_price}
+              *
+            </Card.Text>
+          )}
+          <div className="btn-group">
+            <div className="d-none d-xl-block">
+              <input
+                type="number"
+                min="1"
+                name="quantity"
+                value={itemCount}
+                className="form-control input-number"
+                // eslint-disable-next-line radix
+                onChange={(e) => setItemCount(parseInt(e.target.value))}
+                style={{
+                  maxWidth: '70px',
+                }}
+              />
+            </div>
+            <Button
+              variant="primary"
+              id="main-add-btn"
+              data-testid={`main-add-btn-${product.id}`}
+              onClick={(e) => handleAddItemToCart(e, product.id, product.price)}
+            >
+              Añadir
+              {' '}
+              <FontAwesomeIcon icon={faShoppingBag} />
+            </Button>
+            <Link
+              to={{
+                pathname: `/products/${product.id}`,
+              }}
+              id="main-details-btn"
+            >
+              <Button variant="secondary">Info</Button>
+            </Link>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 });
 export default ProductCard;
