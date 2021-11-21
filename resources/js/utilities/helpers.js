ed5eaef269c8192ae740560dd08be03f324c3e5e
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2021-11-20 18:44:51 -0600

    get image for error handling fn

--- resources/js/utilities/helpers.js
+++ resources/js/utilities/helpers.js
@@ -1,15 +1,32 @@
-export function debounce (func, wait, inmmediate) {
-    var timeout;
-    return function () {
-        var context = this;
-        var args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!inmmediate) func.apply(context, args);
-        };
-        var callNow = inmmediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    }
-};
+/* eslint-disable prefer-rest-params */
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+/* eslint-disable import/prefer-default-export */
+export function debounce(func, wait, inmmediate) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!inmmediate) func.apply(context, args);
+    };
+    const callNow = inmmediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+export const getProductImage = (productId) => (
+  <Card.Img
+    src={`/img/products/${productId}.jpg`}
+    style={{
+      width: '85px',
+      height: '85px',
+    }}
+    onError={(e) => { e.target.onerror = null; e.target.src = '/img/products/no-product.jpg'; }}
+    alt={`Product Id ${productId}`}
+  />
+);
