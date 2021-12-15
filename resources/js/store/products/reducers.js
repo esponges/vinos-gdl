dbbade58ea9118ae1b3cd50d1c9949728ddbeb29
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2021-12-14 21:03:40 -0600

    get product reducer+action

--- resources/js/store/products/reducers.js
+++ resources/js/store/products/reducers.js
@@ -1,25 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit'
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const productsSlice = createSlice({
-    name: 'products',
-    initialState: {
-        loading: 'idle',
-        products: []
+  name: 'products',
+  initialState: {
+    loading: 'idle',
+    products: [],
+  },
+  reducers: {
+    productsLoading: (state) => {
+      if (state.loading === 'idle') {
+        state.loading = 'pending';
+      }
     },
-    reducers: {
-        productsLoading: (state) => {
-            if (state.loading === 'idle') {
-                state.loading = 'pending';
-            }
-        },
-        productsAdded: (state, action) => {
-            if (state.loading === 'pending') {
-                state.loading = 'idle';
-                state.products = (action.payload);
-            }
-        }
-    }
+    productsAdded: (state, action) => {
+      if (state.loading === 'pending') {
+        state.loading = 'idle';
+        state.products = (action.payload);
+      }
+    },
+    productAdded: (state, action) => {
+      const index = state.products.findIndex((product) => product.id === action.payload.id);
+      state.products[index] = {
+        ...state.products[index],
+        ...action.payload,
+      };
+    },
+  },
 });
 
-export const { productsAdded, productsLoading } = productsSlice.actions;
+export const { productsAdded, productsLoading, productAdded } = productsSlice.actions;
 export default productsSlice.reducer;
+
+export const getProduct = (id) => async (dispatch) => {
+  dispatch(productsLoading());
+  const { data } = await axios.get(`/products/${id}`);
+  dispatch(productAdded(data));
+  return data;
+};
