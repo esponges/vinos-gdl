4346c58ce10c9352e61ac82ce46145d0346cd673
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2021-12-15 21:14:01 -0600

    new screenSize hook

--- resources/js/components/controls/hooks/misc.js
+++ resources/js/components/controls/hooks/misc.js
@@ -49,11 +49,52 @@ export const useAddItemToCart = () => {
   const dispatch = useDispatch();
   const context = useContext(Context);
 
-  const handleAddItemToCart = (id, itemCount = 1, price) => {
+  const handleAddItemToCart = (id, price, itemCount = 1) => {
     dispatch(addItemToCart(id));
+
     const subTotal = price * itemCount;
     context.notifyMinAmountRemaining(subTotal);
   };
 
   return handleAddItemToCart;
+};
+
+const getDeviceScreenSize = (width) => {
+  if (width < 415) {
+    return 'mobile';
+  }
+  if (width < 469) {
+    return 'tablet';
+  }
+  return 'desktop';
+};
+
+export const useWindowSize = () => {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+    screenType: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        screenType: getDeviceScreenSize(window.innerWidth),
+        isMobile: getDeviceScreenSize(window.innerWidth) === 'mobile',
+      });
+    }
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  // console.log('windowsize', windowSize);
+  return { ...windowSize };
 };
