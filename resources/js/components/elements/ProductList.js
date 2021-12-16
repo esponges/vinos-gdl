20cec0cf8d3911a9ed8147e5543f0e3bece67478
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2021-12-16 13:35:31 -0600

    update table to allow horizontal overflow

--- resources/js/components/elements/ProductList.js
+++ resources/js/components/elements/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, cloneElement } from 'react';
 import { useSelector } from 'react-redux';
 import _ from 'lodash';
 import { Table, Column, AutoSizer } from 'react-virtualized';
@@ -15,6 +15,18 @@ import {
 
 const defaultParams = {
   sortBy: 'updatedAt', sortDir: 'DESC', search: '', page: '1',
+};
+
+// append style to autoSizer without breaking it by using style prop directly
+const originalRender = AutoSizer.prototype.render;
+const autoOverflow = { overflow: 'auto' };
+AutoSizer.prototype.render = function render() {
+  const { allowHorizontalScroll } = this.props;
+  const renderedElement = originalRender.call(this);
+
+  return allowHorizontalScroll
+    ? cloneElement(renderedElement, { style: autoOverflow })
+    : renderedElement;
 };
 
 const ProductList = ({ history, location }) => {
@@ -44,17 +56,23 @@ const ProductList = ({ history, location }) => {
         <Form.Label>Búsqueda</Form.Label>
         <Form.Control type="text" placeholder="¿Qué buscas?" value={urlParams.search} onChange={onSearchChange} />
       </Form.Group>
-      <AutoSizer>
+      <AutoSizer allowHorizontalScroll>
         {({ width }) => (
           <Table
-            width={width}
+            width={!isMobile ? width : width * 1.2}
             height={window.innerHeight - 300}
-            headerHeight={20}
+            headerHeight={60}
             rowHeight={60}
             rowCount={areProductsSet ? productsListToUse.length : 0}
             rowGetter={areProductsSet ? ({ index }) => productsListToUse[index] : () => { }}
             sortDirection={urlParams.sortDir}
           >
+            <Column
+              width={width * (!isMobile ? 0.1 : 0.1)}
+              label=""
+              dataKey=""
+              cellRenderer={({ rowData: { id, price } }) => listActionsRenderer(id, price, handleAddItemToCart, isMobile)}
+            />
             <Column
               width={width * 0.1 * (isMobile ? 1.65 : 1)}
               label="Precio"
@@ -64,7 +82,7 @@ const ProductList = ({ history, location }) => {
             <Column
               label="Nombre"
               dataKey="name"
-              width={width * 0.5 * (isMobile ? 1.2 : 1)}
+              width={width * 0.5 * (isMobile ? 1.5 : 1)}
               cellRenderer={nameLinkRenderer}
             />
             {!isMobile
@@ -76,12 +94,6 @@ const ProductList = ({ history, location }) => {
                   cellRenderer={({ cellData }) => renderCategory(cellData, categories)}
                 />
               )}
-            <Column
-              width={width * 0.2}
-              label=""
-              dataKey=""
-              cellRenderer={({ rowData: { id, price } }) => listActionsRenderer(id, price, handleAddItemToCart, isMobile)}
-            />
           </Table>
         )}
       </AutoSizer>
