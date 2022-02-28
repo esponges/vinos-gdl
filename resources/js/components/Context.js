564d3e79c6e612ee7268dbf77ed418ae6598c1b2
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2022-02-28 09:45:20 -0600

    improve context

--- resources/js/components/Context.js
+++ resources/js/components/Context.js
@@ -1,3 +1,17 @@
 import { createContext } from 'react';
 
 export const Context = createContext();
+
+const ContextProvider = ({ children }) => {
+    return <Context value={value}>{children}</Context>
+};
+
+const useAppContext = () => {
+    const context = React.useContext(Context);
+    if(context === undefined) {
+        throw new Error('useAppContext must be used within a ContextProvider');
+    }
+    return context;
+};
+
+export {ContextProvider, useAppContext}
